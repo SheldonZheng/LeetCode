4eda4db577ba5635ddb40322cf34a8a0525f1290
unknown <>
2016-05-31 17:15:01 +0800

    Add ReverseString.java-LeetCode-344

---
+++ ReverseString.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public String reverseString(String s) {
+        int length = s.length();
+		if(length <= 1)
+			return s;
+		String left = s.substring(0,length / 2);
+		String right = s.substring(length / 2,length);
+		return reverseString(right) + reverseString(left);
+    }
+}
