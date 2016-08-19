45ed91be9bb6245dc1ee46636f42d570178c2477
Baiye <zhenghangtxdyr@gmail.com>
2016-08-19 10:41:02 +0800

    解决LeetCode103题，Zig-Zag层次顺序遍历二叉树（非递归）；

--- BinaryTreeZigzagLevelOrderTraversal.java
+++ BinaryTreeZigzagLevelOrderTraversal.java
@@ -9,6 +9,10 @@ import java.util.*;
  * Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
  *
  * 给入一个二叉树，通过Zig-Zag遍历返回一个二层的List；
+ *
+ * 思路是构造一个队列，先放入root节点，第一次遍历把root节点的左右节点放进去，下一次遍历就是root节点的左右节点的子节点……依次类推；
+ * 因为是Zig-Zag遍历，所以放置一个flag来判断是不是要翻转结果；
+ * 注意开头的root==null的判断；
  */
 
 public class BinaryTreeZigzagLevelOrderTraversal {
