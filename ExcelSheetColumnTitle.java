499a1224c4d8bc4c6e4f35583d46ab0ae7ebc566
Baiye <zhenghangtxdyr@gmail.com>
2016-08-24 11:22:58 +0800

    解决LeetCode168题，实质上是转换十进制到十六进制；

---
+++ ExcelSheetColumnTitle.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Baiye on 2016/8/24.
+ */
+
+/**
+ * LeetCode.168.Excel Sheet Column Title
+ * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+ */
+
+public class ExcelSheetColumnTitle {
+    public String convertToTitle(int n) {
+        String result = "";
+        while(n>0){
+            result = (char)((n-1)%26 +'A') + result;
+            n = (n-1)/26;
+        }
+        return result;
+
+
+    }
+
+
+}
