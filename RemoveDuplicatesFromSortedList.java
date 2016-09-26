35494eca18d2305ebc833ba229b858172bde9b03
Baiye <zhenghangtxdyr@gmail.com>
2016-09-26 11:12:51 +0800

    解决LeetCode83题，有序链表去重；

---
+++ RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,34 @@
+/**
+ * Created by Baiye on 2016/9/26.
+ */
+
+/**
+ * LeetCode.83.Remove Duplicates from Sorted List
+ * 有序链表去重
+ */
+public class RemoveDuplicatesFromSortedList {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null || head.next == null)
+            return head;
+
+        ListNode h = new ListNode(0);
+        h.next = head;
+
+        for(ListNode tempNode = h;tempNode.next != null; tempNode = tempNode.next)
+        {
+            while(tempNode.next.next != null && tempNode.next.val == tempNode.next.next.val)
+            {
+                tempNode.next = tempNode.next.next;
+            }
+        }
+
+        return h.next;
+    }
+
+      private class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) { val = x; }
+      }
+}
