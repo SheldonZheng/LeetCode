31d95bd9b6ff5fdc63c891450c89b118357b1e7a
Baiye <zhenghangtxdyr@gmail.com>
2016-12-15 14:56:04 +0800

    解决LeetCode112题，判断当前是否满足，然后减掉当前节点的值分开两边递归;

---
+++ BinaryTree/PathSum.java
@@ -0,0 +1,26 @@
+package BinaryTree;
+
+/**
+ * Created by Baiye on 2016/12/15.
+ */
+
+/**
+ * LeetCode.112.Path Sum
+ */
+public class PathSum {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null)
+            return false;
+        if(root.left == null && root.right == null && root.val == sum)
+            return true;
+        return hasPathSum(root.left,sum - root.val) || hasPathSum(root.right,sum - root.val);
+    }
+
+    private class TreeNode {
+             int val;
+             TreeNode left;
+             TreeNode right;
+             TreeNode(int x) { val = x; }
+         }
+}
