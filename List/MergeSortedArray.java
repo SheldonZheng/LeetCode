f10d2df10ea21799bf2ef034b9b31d8405e90e6b
SheldonZheng <zhenghangtxdyr@gmail.com>
2017-03-31 10:24:07 +0800

    解决LeetCode.88,从M+N,也就是最后生成新数组的最高位向前添加数;类似归并排序的合并;

---
+++ List/MergeSortedArray.java
@@ -0,0 +1,29 @@
+package List;
+
+/**
+ * @author Baiye
+ * @since 2017-03-31
+ */
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int k = m + n - 1;
+        int i = m - 1;
+        int j = n - 1;
+
+        while (k >= 0) {
+            if (i < 0) {
+                nums1[k--] = nums2[j--];
+                continue;
+            }
+            if (j < 0) {
+                break;
+            }
+
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+}
