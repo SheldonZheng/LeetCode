a5b6d2487f873d595cd6e3ea00523cbc45b39cd2
SheldonZheng <zhenghangtxdyr@gmail.com>
2018-08-28 10:24:18 +0800

    Solve Reverse Bits

---
+++ ReverseBits.java
@@ -0,0 +1,15 @@
+// LeetCode 190
+public class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & 1) == 1) {
+                result = (result << 1) + 1;
+            } else {
+                result = result << 1;
+            }
+            n >>= 1;
+        }
+        return result;
+    }
+}
