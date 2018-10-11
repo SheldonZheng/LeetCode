98185ad49fea5b0dd267289908653d2c3c2cfaa3
SheldonZheng <zhenghangtxdyr@gmail.com>
2018-10-11 11:35:52 +0800

    Solve Toeplitz Matrix

---
+++ ToeplitzMatrix.java
@@ -0,0 +1,16 @@
+//LeetCode 766
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        if (matrix == null) {
+            return false;
+        }
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[i].length - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
