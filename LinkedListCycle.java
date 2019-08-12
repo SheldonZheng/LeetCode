77caa5a4f3abf8ad55c999d1afc3ddb6e4540f59
SheldonZheng <zhenghangtxdyr@gmail.com>
2019-08-12 17:44:34 +0800

    Linked List Cycle 2

--- LinkedListCycle.java
+++ LinkedListCycle.java
@@ -6,11 +6,11 @@
  * LeetCode.141.Linked List Cycle
  */
 public class LinkedListCycle {
-    public boolean hasCycle(ListNode head) {
+ a   public boolean hasCycle(ListNode head) {
         ListNode p1 = head;
         ListNode p2 = head;
 
-        while (p1 != null && p2 != null && p2.next != null) {
+  a      while (p1 != null && p2 != null && p2.next != null) {
             p1 = p1.next;
             p2 = p2.next.next;
 
@@ -19,7 +19,24 @@ public class LinkedListCycle {
         }
 
         return false;
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
     }
+}
+
+
 
     private class ListNode {
         int val;
