499f6cc605605493689b34b3786707adbfbd0c75
SheldonZheng <zhenghangtxdyr@gmail.com>
2019-08-15 16:14:19 +0800

    Symmetric Tree

---
+++ SymmetricTree.java
@@ -0,0 +1,30 @@
+/**
+ * LeetCode 101 Symmetric Tree
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return mirror(root,root);
+    }
+
+    public boolean mirror(TreeNode left,TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return left.val == right.val && mirror(left.left,right.right) && mirror(left.right,right.left);
+    }
+}
+
+
