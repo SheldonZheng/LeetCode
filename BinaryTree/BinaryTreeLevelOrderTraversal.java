6b2b3449e25ddf3e7b739aabc31314e4ae98ded2
SheldonZheng <zhenghangtxdyr@gmail.com>
2019-08-21 15:51:29 +0800

    Binary Tree Level Order Traversal

--- BinaryTree/BinaryTreeLevelOrderTraversal.java
+++ BinaryTree/BinaryTreeLevelOrderTraversal.java
@@ -8,7 +8,7 @@ import java.util.*;
  *
  * LeetCode.102.Binary Tree Level Order Traversal
  * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
- *
+ * 2019/08/21
  * 很简单的层级顺序遍历，思路是构造一个队列，先放入root节点，第一次遍历把root节点的左右节点放进去，下一次遍历就是root节点的左右节点的子节点……依次类推；
  */
 public class BinaryTreeLevelOrderTraversal {
