6cb5768d7465f711fb6d8aae785d658a121adc94
SheldonZheng <zhenghangtxdyr@gmail.com>
2019-10-09 15:27:57 +0800

    Find Peak Element

---
+++ FindPeakElement.java
@@ -0,0 +1,13 @@
+/**
+ * LeetCode 162 Find Peak Element
+ */
+class Solution {
+    public int findPeakElement(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                return i;
+            }
+        }
+        return nums.length - 1;
+    }
+}
