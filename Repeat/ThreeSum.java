0bafdbe2c56820ea4b13b79c99ee58f9081ea999
SheldonZheng <zhenghangtxdyr@gmail.com>
2020-02-26 11:19:48 +0800

    Three Sum

---
+++ Repeat/ThreeSum.java
@@ -0,0 +1,41 @@
+/**
+ * LeetCode 15 3Sum Repeat
+ */
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    while (l < r && nums[l + 1] == nums[l]) {
+                        l++;
+                    }
+                    while (l < r && nums[r - 1] == nums[r]) {
+                        r--;
+                    }
+                    l++;
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return result;
+    }
+}
