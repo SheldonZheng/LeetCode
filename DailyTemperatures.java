795ddfb2a265ff7750472fce2f408ed2eff41db6
SheldonZheng <zhenghangtxdyr@gmail.com>
2020-06-11 10:58:06 +0800

    Daily Temperatures

---
+++ DailyTemperatures.java
@@ -0,0 +1,20 @@
+/**
+ * LeetCode 739 Daily Temperatures
+ */
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        if (T == null || T.length == 0) {
+            return new int[0];
+        }
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[T.length];
+        for (int i = 0; i < T.length; i++) {
+            while (!stack.isEmpty() && T[i] > T[stack.peek()]) {
+                int t = stack.pop();
+                res[t] = i - t;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
