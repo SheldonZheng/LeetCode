c014221352d52b9990533074db9d8d33c552db58
SheldonZheng <zhenghangtxdyr@gmail.com>
2020-06-12 10:06:02 +0800

    Maximum Product of Two Elements in an Array

---
+++ MaximumProductOfTwoElementsInAnArray.java
@@ -0,0 +1,9 @@
+/**
+ * LeetCode Maximum Product of Two Elements in an Array
+ */
+class Solution {
+    public int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+        return (nums[nums.length - 1] - 1) * (nums[nums.length - 2] - 1);
+    }
+}
