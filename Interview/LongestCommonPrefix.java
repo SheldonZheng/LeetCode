c46de1b4f474fc190a496fe77371414c815b72e4
SheldonZheng <zhenghangtxdyr@gmail.com>
2020-06-15 10:18:57 +0800

    Longest Common Prefix

---
+++ Interview/LongestCommonPrefix.java
@@ -0,0 +1,20 @@
+/**
+ * LeetCode 14 Longest Common Prefix
+ */
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String pre = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(pre)) {
+                pre = pre.substring(0, pre.length() - 1);
+                if (pre.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return pre;
+    }
+}
