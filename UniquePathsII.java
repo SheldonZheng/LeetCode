9613c183193ad631d9ba90ab352790974dcce57a
SheldonZheng <zhenghangtxdyr@gmail.com>
2020-07-06 11:07:53 +0800

    Unique Paths II

---
+++ UniquePathsII.java
@@ -0,0 +1,27 @@
+/**
+ * LeetCode 63 Unique Paths II
+ */
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0) {
+            return 0;
+        }
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+        int[] dp = new int[m];
+
+        dp[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0;
+                    continue;
+                }
+                if (j >= 1 && obstacleGrid[i][j - 1] == 0) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[m - 1];
+    }
+}
