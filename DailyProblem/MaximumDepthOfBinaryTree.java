38ae36dfe858800d6f7c620ac99bcee00516a164
SheldonZheng <zhenghangtxdyr@gmail.com>
2020-07-28 10:54:09 +0800

    Maximum Depth of Binary Tree

---
+++ DailyProblem/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,9 @@
+/**
+ * LeetCode 104 Maximum Depth of Binary Tree
+ */
+public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left),maxDepth(root.right)) + 1;
+}
