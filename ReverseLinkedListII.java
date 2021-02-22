f911e4ba26cf54b82d066478651ecab71f8ece4e
SheldonZheng <zhenghangtxdyr@gmail.com>
2021-02-22 11:47:47 +0800

    Reverse Linked List II

---
+++ ReverseLinkedListII.java
@@ -0,0 +1,36 @@
+/**
+ * LeetCode Reverse Linked List II
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m == 1) {
+            return reverseN(head,n);
+        }
+        head.next = reverseBetween(head.next,m - 1,n - 1);
+        return head;
+    }
+
+    ListNode backNode = null;
+
+    public ListNode reverseN(ListNode head,int n) {
+        if (n == 1) {
+            backNode = head.next;
+            return head;
+        }
+        ListNode last = reverseN(head.next,n - 1);
+        head.next.next = head;
+        head.next = backNode;
+        return last;
+    }
+
+}
