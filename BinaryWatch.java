275657ea25c350c43d81d48ff340e9670ff02951
SheldonZheng <zhenghangtxdyr@gmail.com>
2021-06-21 10:50:53 +0800

    Binary Watch

---
+++ BinaryWatch.java
@@ -0,0 +1,13 @@
+/**
+ * LeetCode 401 Binary Watch
+ */
+func readBinaryWatch(turnedOn int) (ans []string) {
+	for h := uint8(0); h < 12 ;h++ {
+		for m := uint8(0); m < 60 ;m++ {
+			if bits.OnesCount8(h) + bits.OnesCount8(m) == turnedOn {
+				ans = append(ans,fmt.Sprintf("%d:%02d",h,m))
+			}
+		}
+	}
+	return ans
+}
