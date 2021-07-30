85361650a91e282d4d41b62a1e03a3e8290b589b
SheldonZheng <zhenghangtxdyr@gmail.com>
2021-07-30 10:26:26 +0800

    Excel Sheet Column Number

---
+++ ExcelSheetColumnNumber.go
@@ -0,0 +1,12 @@
+/**
+ * LeetCode 171 Excel Sheet Column Number
+ */
+func titleToNumber(columnTitle string) (number int) {
+	for i, multiple := len(columnTitle) - 1,1 ; i >= 0;i-- {
+		k := columnTitle[i] - 'A' + 1
+		number += int(k) * multiple
+		multiple *= 26
+	}
+	return number
+}
+
