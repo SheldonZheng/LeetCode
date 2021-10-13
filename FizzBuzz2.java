d844ce589be5ed32bf202ee1ba10233f91ac3edf
SheldonZheng <zhenghangtxdyr@gmail.com>
2021-10-13 10:04:20 +0800

    Fizz Buzz

---
+++ FizzBuzz2.java
@@ -0,0 +1,22 @@
+/**
+ * LeetCode 412 Fizz Buzz
+ */
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<String>();
+        for (int i = 1; i <= n; i++) {
+            StringBuffer sb = new StringBuffer();
+            if (i % 3 == 0) {
+                sb.append("Fizz");
+            }
+            if (i % 5 == 0) {
+                sb.append("Buzz");
+            }
+            if (sb.length() == 0) {
+                sb.append(i);
+            }
+            answer.add(sb.toString());
+        }
+        return answer;
+    }
+}
