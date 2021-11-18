2e83305f7e9b7fa0b30719daa6532c5b8c7a9bf9
SheldonZheng <zhenghangtxdyr@gmail.com>
2021-11-18 10:41:53 +0800

    Binary Tree Tilt

---
+++ BinaryTreeTilt.java
@@ -0,0 +1,36 @@
+/**
+ * LeetCode 563 Binary Tree Tilt
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int res = 0;
+
+    public int findTilt(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int sl = dfs(root.left);
+        int sr = dfs(root.right);
+        res += Math.abs(sl - sr);
+        return sl + sr + root.val;
+    }
+}
