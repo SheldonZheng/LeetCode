08d43bc9785a6b0e6abfaa46849d3450db5ec26c
SheldonZheng <zhenghangtxdyr@gmail.com>
2022-01-11 11:03:38 +0800

    Decode Ways

---
+++ InterviewPrepare/DecodeWays.java
@@ -0,0 +1,22 @@
+/**
+ * LeetCode 91 Decode Ways
+ */
+class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= s.length(); i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+            if (i > 1 && s.charAt(i - 2) != '0'
+                    && ((s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0') <= 26)) {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[s.length()];
+    }
+}
