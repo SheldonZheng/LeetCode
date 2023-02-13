377731b7db0b461f5352fde2fb75a3fc5a59ee3e
SheldonZheng <zhenghangtxdyr@gmail.com>
2023-02-13 10:50:42 +0800

    Replace the Substring for Balanced String

---
+++ ReplaceTheSubstringForBalancedString.go
@@ -0,0 +1,39 @@
+func balancedString(s string) int {
+	cnt := map[byte]int{}
+	for _,c := range s {
+		cnt[byte(c)]++
+	}
+	partial := len(s) / 4
+	check := func() bool {
+		if cnt['Q'] > partial || cnt['W'] > partial || cnt['E'] > partial || cnt['R'] > partial {
+			return false
+		}
+		return true
+	}
+
+	if check() {
+		return 0
+	}
+	
+	res := len(s)
+	r := 0
+	for l,c := range s {
+		for r < len(s) && !check() {
+			cnt[s[r]]--
+			r += 1
+		}
+		if !check() {
+			break
+		}
+		res = min(res,r - l)
+		cnt[byte(c)]++
+	}
+	return res
+}
+
+func min(a int,b int) int {
+	if (a < b) {
+		return a
+	}
+	return b
+}
