f198776538763f08e90032eef14eab6edfd3bf7e
SheldonZheng <zhenghangtxdyr@gmail.com>
2023-03-01 10:58:51 +0800

    Largest Local Values in a Matrix

---
+++ LargestLocalValuesInAMatrix.go
@@ -0,0 +1,20 @@
+func largestLocal(grid [][]int) [][]int {
+	n := len(grid)
+	res := make([][]int,n - 2)
+	for i := 1;i < n - 1;i++ {
+		row := make([]int,n - 2)
+		for j := 1;j < n - 1;j++ {
+			mx := grid[i][j]
+			for r := i - 1;r <= i + 1;r++ {
+				for c := j - 1;c <= j + 1;c++ {
+					if grid[r][c] > mx {
+						mx = grid[r][c]
+					}
+				}
+			}
+			row[j - 1] = mx
+		}
+		res[i - 1] = row
+	}
+	return res
+}
