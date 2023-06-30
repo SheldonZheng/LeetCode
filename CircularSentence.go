aeddaec74fc4c88a8863430a2496e2fd23079f3f
SheldonZheng <zhenghangtxdyr@gmail.com>
2023-06-30 10:06:53 +0800

    Circular Sentence

---
+++ CircularSentence.go
@@ -0,0 +1,12 @@
+func isCircularSentence(sentence string) bool {
+	n := len(sentence)
+	if sentence[0] != sentence[n - 1] {
+		return false
+	}
+	for i := 0;i < n;i++ {
+		if sentence[i] == ' ' && sentence[i + 1] != sentence[i - 1] {
+			return false
+		}
+	}
+	return true
+}
