29429d1bbaef349ebef789a492874cc6bfd2c298
SheldonZheng <zhenghangtxdyr@gmail.com>
2023-08-21 10:38:17 +0800

    Move Pieces to Obtain a String

---
+++ MovePiecesToObtainAString.go
@@ -0,0 +1,36 @@
+func canChange(start string, target string) bool {
+	i, j, n := 0,0, len(start)
+	for i < n && j < n {
+		for i < n && start[i] == '_' {
+			i++
+		}
+		for j < n && target[j] == '_' {
+			j++
+		}
+		if i < n && j < n {
+			if start[i] != target[j] {
+				return false
+			}
+			c := start[i]
+			if (c == 'L' && i < j) || (c == 'R' && i > j)  {
+				return false
+			}
+			i++
+			j++
+		}
+	}
+
+	for i < n {
+		if start[i] != '_' {
+			return false
+		}
+		i++
+	}
+	for j < n {
+		if target[j] != '_' {
+			return false
+		}
+		j++
+	}
+	return true
+}
