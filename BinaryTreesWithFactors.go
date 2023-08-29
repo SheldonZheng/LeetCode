3492c148775a1eea2892934b2358dd473cb652ce
SheldonZheng <zhenghangtxdyr@gmail.com>
2023-08-29 11:02:19 +0800

    Binary Trees With Factors

---
+++ BinaryTreesWithFactors.go
@@ -0,0 +1,26 @@
+func numFactoredBinaryTrees(arr []int) int {
+	const mod int = 1e9 + 7
+	sort.Ints(arr)
+	f := make([]int, len(arr))
+	idx := map[int]int{}
+	for i, v := range arr {
+		f[i] = 1
+		idx[v] = i
+	}
+
+	for i, a := range arr {
+		for j := 0;j < i;j++ {
+			b := arr[j]
+			if c := a / b; a % b == 0 {
+				if k, ok := idx[c]; ok {
+					f[i] = (f[i] + f[j] * f[k]) % mod
+				}
+			}
+		}
+	}
+	res := 0
+	for _,v := range f {
+		res = (res + v) % mod
+	}
+	return res
+}
