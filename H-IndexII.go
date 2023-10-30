41e3bee7677f60dbaa3fc83ea5223565d4fa1f8d
SheldonZheng <zhenghangtxdyr@gmail.com>
2023-10-30 12:05:04 +0800

    H-Index II

--- H-IndexII.go
+++ H-IndexII.go
@@ -8,3 +8,16 @@ func hIndex(citations []int) int {
     n := len(citations)
     return n - sort.Search(n,func (x int) bool { return citations[x] >= n - x})
 }
+func hIndex(citations []int) int {
+	n := len(citations)
+	l ,r := 0, n
+	for l < r {
+		mid := (l + r + 1) / 2
+		if citations[n - mid] >= mid {
+			l = mid
+		} else {
+			r = mid - 1
+		}
+	}
+	return l
+}
