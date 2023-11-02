b4141adc59d6ea8ec4c706b536f5b4bb4fce5a22
SheldonZheng <zhenghangtxdyr@gmail.com>
2023-11-02 17:17:44 +0800

    Rings and Rods

---
+++ RingsAndRods.go
@@ -0,0 +1,17 @@
+func countPoints(rings string) int {
+	res := 0
+	d := ['Z']int{'R':1, 'G':2, 'B':4}
+	mask := [10]int{}
+	for i, n := 0, len(rings); i < n;i += 2 {
+		c := rings[i]
+		j := int(rings[i + 1] - '0')
+		mask[j] |= d[c]
+	}
+
+	for _, x := range mask {
+		if x == 7 {
+			res++
+		}
+	}
+	return res
+}
